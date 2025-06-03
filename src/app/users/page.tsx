be5911ee0da738932f6683c33420b5010a53af85
steve3github <badayosethan@gmail.com>
2025-06-03 18:46:13 +0800

    additional stuff ig

---
+++ src/app/users/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+interface User {
+   id: number
+   name: string
+}
+
+const UsersPage = async () => {
+   const res = await fetch('https://jsonplaceholder.typicode.com/users', 
+      { next: { revalidate: 10}})
+   const users: User[] = await res.json()
+   return (
+      <main>
+         <h1>Users</h1>
+         <p>{new Date().toLocaleTimeString()}</p>
+         <ul>
+            {users.map(user => <li key={user.id}>{user.name}</li>)}
+         </ul>
+      </main>
+   )
+}
+
+export default UsersPage
